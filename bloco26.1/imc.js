e7507db2fc1230d8a150066ef6d5123b77ab4645
Tiago Santos <tiago88q@gmail.com>
2021-08-25 12:31:07 -0300

    requirement:3

--- bloco26.1/imc.js
+++ bloco26.1/imc.js
@@ -1,6 +1,12 @@
+const rdl = require('readline-sync');
+
 function calculaImc(peso, altura) {
+  peso = rdl.question(`Qual seu peso?  `);
+  altura = rdl.question(`Qual sua altura?  `);
+
   let imc = peso / (altura * altura);
+
   console.log(imc);
 }
 
-calculaImc(58, 1.67);
+calculaImc();
