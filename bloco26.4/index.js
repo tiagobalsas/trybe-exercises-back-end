5df700df41a0f884cd7af174741bbd28e4c0d600
Tiago Santos <tiago881@gmail.com>
2021-09-06 20:33:18 -0300

    Update

--- bloco26.4/index.js
+++ bloco26.4/index.js
@@ -14,7 +14,7 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
-app.get('/hello', (req, res) => {
+app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({ message: `Hello, ${name}` });
 });
