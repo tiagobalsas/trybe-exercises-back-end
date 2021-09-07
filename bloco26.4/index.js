640b8bafb2869d051bcbe9771fab6db697468112
Tiago Santos <tiago881@gmail.com>
2021-09-06 23:09:38 -0300

    Update

--- bloco26.4/index.js
+++ bloco26.4/index.js
@@ -6,9 +6,6 @@ const HTTP_OK = 200;
 const PORT = 3001;
 app.use(bodyParser.json());
 
-app.use((err, req, res, next) => {
-  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
-});
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
@@ -21,12 +18,15 @@ app.post('/hello', (req, res) => {
 
 app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
-
+  
   if (parseInt(age, 10) <= 17) {
     return res.status(401).json({ message: `Unauthorized` });
   }
-
+  
   res.status(200).json({ message: `Hello, ${name}!` });
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+});
 app.listen(PORT, HTTP_OK, () => console.log(`Server Online -> PORT ${PORT}`));
