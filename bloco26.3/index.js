b5634bf09bc378e146fa71ef2abe5938f0ce9089
Tiago Santos <tiago881@gmail.com>
2021-11-22 14:17:43 -0300

    feat(API/Restfull)

---
+++ bloco26.3/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser ');
+
+const app = express();
+app.use(app.use(bodyParser.json()));
+
+app.use('/products', require('./controllers/productController'));
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});
